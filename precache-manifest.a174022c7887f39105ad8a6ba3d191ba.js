862f539f0ff72db8ae626b6365a37eaa318e4b46
Erin Smith <erin.acumen@gmail.com>
2020-08-22 23:41:48 -0700

    Updates

---
+++ precache-manifest.a174022c7887f39105ad8a6ba3d191ba.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55db2a045e8ff62cf5a0023a8a88a10f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ab9c1a071dfe7027f051",
+    "url": "/react-portfolio/static/css/main.252b1f1e.chunk.css"
+  },
+  {
+    "revision": "c2a20ad901d13300cd1a",
+    "url": "/react-portfolio/static/js/2.ebe7a56a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.ebe7a56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9c1a071dfe7027f051",
+    "url": "/react-portfolio/static/js/main.8e6247c5.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b7a36e9949e62f86f53286d54c55c70e",
+    "url": "/react-portfolio/static/media/berry.b7a36e99.jpg"
+  },
+  {
+    "revision": "bfd8c8c2a6d83aa797afd77540cac9f2",
+    "url": "/react-portfolio/static/media/burgerShot.bfd8c8c2.png"
+  },
+  {
+    "revision": "47384aacc5cc1d5806f53e0be995073d",
+    "url": "/react-portfolio/static/media/cards.47384aac.png"
+  },
+  {
+    "revision": "a230ee56a9f15356df278974bc9b6ab8",
+    "url": "/react-portfolio/static/media/erin.a230ee56.jpg"
+  },
+  {
+    "revision": "2da70cf7673738bffdde7ebcb6c13bf6",
+    "url": "/react-portfolio/static/media/game.2da70cf7.png"
+  },
+  {
+    "revision": "35544af90269aa1a3ba672c5b1f3efd0",
+    "url": "/react-portfolio/static/media/latte.35544af9.jpg"
+  },
+  {
+    "revision": "caf76055cabd4ffc7278f30609dcd5ca",
+    "url": "/react-portfolio/static/media/loo_logo.caf76055.jpg"
+  },
+  {
+    "revision": "bbb0ad2661549bada63c55b3020aa818",
+    "url": "/react-portfolio/static/media/myfirstpage.bbb0ad26.png"
+  },
+  {
+    "revision": "af5323cb33be732a60ca6cb6bba881e4",
+    "url": "/react-portfolio/static/media/notepad.af5323cb.png"
+  },
+  {
+    "revision": "54c27768760e4ea1391ed270065dba2b",
+    "url": "/react-portfolio/static/media/passwordgen.54c27768.png"
+  },
+  {
+    "revision": "978e698940029492a0cbb34af96002e9",
+    "url": "/react-portfolio/static/media/profile.978e6989.JPG"
+  },
+  {
+    "revision": "74239da2b4bf1227acb41226c355a683",
+    "url": "/react-portfolio/static/media/purplepages.74239da2.png"
+  },
+  {
+    "revision": "08c1d0e902a698bcbf822c62510e6ce3",
+    "url": "/react-portfolio/static/media/quiz.08c1d0e9.png"
+  },
+  {
+    "revision": "f4ae7f2a8711be748f4ee2ea584daf5a",
+    "url": "/react-portfolio/static/media/scheduler.f4ae7f2a.png"
+  },
+  {
+    "revision": "3bff9b8e28aca7ef13b9d341ab4b5119",
+    "url": "/react-portfolio/static/media/team.3bff9b8e.png"
+  }
+]);
--- react-portfolio/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-
-export default {
-  sendMail: function(settings) {
-    return axios.post(settings.url, settings.data);
-  }
-};
